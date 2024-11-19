d0aa152346f91af3af4583caee787642e7fe68cb
Carlos León <mongrelion@gmail.com>
2024-11-19 12:25:24 +0100

    Automatic tf file formatting on save

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -12,3 +12,11 @@ local servers = {
 for _, server in ipairs(servers) do
   lsp[server].setup{}
 end
+
+-- Automatically format terraform files on save
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
